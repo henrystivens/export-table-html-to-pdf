71553fc3f2e976d93ba56c9389c7f1c7001ec644
Henry Adarme <henry.stivens@gmail.com>
2019-01-01 20:15:54 -0500

    Added view with the users table using 'pdf' template

--- default/app/controllers/user_controller.php
+++ default/app/controllers/user_controller.php
@@ -68,4 +68,15 @@ class UserController extends AppController
             return;
         }
     }
+    
+    public function pdf()
+    {
+        //Usa el tempalte 'pdf'
+        View::template('pdf');
+        //Modifica el nombre del archivo a descargar
+        $this->fileName = 'listado-de-usuarios';
+        ////Modifica el título del documento PDF en la cabecera
+        $this->title = 'Listado de usuarios';
+        $this->data = (new User)->find();
+    }
 }
